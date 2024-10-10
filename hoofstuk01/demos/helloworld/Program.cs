07231c492c3ab43e70d43e50508d292908cbf6fc
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-10 16:31:47 +0200

    hfdstk01, demo1 toevoegen

---
+++ hoofstuk01/demos/helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿/* helloworld
+ * 
+ * 2024 - 2025 Patrick Lanove
+ * Electronica/ICT - Programming Principles
+ * 
+ * Hoofdstuk 1, demo 1
+ */
+namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hoi, ik ben het");
+        }
+    }
+}
