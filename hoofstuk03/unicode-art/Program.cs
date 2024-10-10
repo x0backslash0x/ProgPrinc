f2ec033eaf32e7e923e1db8431b6a4e7628d5cf9
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-10 17:33:36 +0200

    labo 3, oefening 5 zet bestand klaar

---
+++ hoofstuk03/unicode-art/Program.cs
@@ -0,0 +1,18 @@
+﻿/* unicde-art
+ * 
+ * 2024 - 2025 Patrick Lanove
+ * Electronica/ICT - Programming Principles
+ * 
+ * Hoofdstuk 3, oefening 5
+ */
+
+namespace unicode_art
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        //code
+    }
+}
+}
