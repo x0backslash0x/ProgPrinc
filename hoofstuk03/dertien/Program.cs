0f7b24fb965a8881188084ef916b87d0db2797e0
Patrick Lanove <patrick.lanove@student.ap.be>
2024-10-10 17:04:34 +0200

    labo 3, oefening 2 wip

---
+++ hoofstuk03/dertien/Program.cs
@@ -0,0 +1,24 @@
+﻿/* dertien
+ * 
+ * 2024 - 2025 Patrick Lanove
+ * Electronica/ICT - Programming Principles
+ * 
+ * Hoofdstuk 3, oefening 2
+ */
+
+namespace dertien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int doosGrootte = 8;
+            int aantalEieren = 124;
+            int aantalDozen = 15;
+            int aantalRest = 4;
+
+            string bericht = $"{aantalEieren} eieren passen in {aantalDozen} dozen van {doosGrootte}. Daarbij zal je nog {aantalRest} eieren over hebben.";
+            Console.WriteLine(bericht);
+        }
+    }
+}
