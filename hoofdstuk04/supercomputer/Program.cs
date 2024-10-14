90eb4749540ed225670ddeef237d6411459339b0
x0backslash0x <paddy0524@gmail.com>
2024-10-14 10:10:01 +0200

    labo 4, oefening 1: project aanmaken

---
+++ hoofdstuk04/supercomputer/Program.cs
@@ -0,0 +1,22 @@
+﻿/* supercomputer
+ * 
+ * 2024 - 2025 Patrick Lanove
+ * Electronica/ICT - Programming Principles
+ * 
+ * Hoofdstuk 4, oefening 1
+ * 
+ * ** OPGAVE **
+ * Vraag aan de gebruiker 3 kommagetallen.
+ * Bereken het gemiddelde van deze 3 getallen en toon dit als een kommagetal op het scherm
+ */
+
+namespace supercomputer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //code
+        }
+    }
+}
