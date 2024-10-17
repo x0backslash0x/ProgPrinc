4f6b2703084791ba118fde0d9dbd774a8ec66f74
x0backslash0x <paddy0524@gmail.com>
2024-10-17 23:37:17 +0200

    labo 1, demo 2 kleurconsole toevoegen

---
+++ hoofstuk01/demos/kleurconsole/Program.cs
@@ -0,0 +1,28 @@
+﻿/* kleurconsole
+ *
+ * 2024 - 2025 Patrick Lanove
+ * Electronica/ICT - Programming Principles
+ *
+ * Hoofdstuk 1, demo 2
+ */
+
+namespace kleurconsole
+{
+    internal class Program
+{
+    static void Main()
+    {
+        // code om de kleuren in de console te veranderen
+        Console.WriteLine("Tekst in de standaard kleur");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("Zwart met gele achtergrond");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Rood met gele achtergrond");
+
+        //kleur resetten
+        Console.ResetColor();
+        Console.WriteLine("Het programma sluit nu af");
+    }
+}
+}
