d2dd5fa524c5759f85148db535b97e16f1ba6f6d
x0backslash0x <paddy0524@gmail.com>
2024-10-18 02:08:11 +0200

    labo 1, oefening 6: aanpassen met functies

--- hoofstuk01/tekening/Program.cs
+++ hoofstuk01/tekening/Program.cs
@@ -12,33 +12,47 @@ namespace tekening
     {
         static void Main(string[] args)
         {
+            static void tekenRoodBlokje(string spacer)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(spacer);
+            }
+
+            static void tekenGroenBlokje(string spacer)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(spacer);
+            }
+
+            static void tekenRodeLijn(string spacer)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tekenRoodBlokje(spacer);
+                }
+                Console.WriteLine("");
+            }
+
+            static void tekenRoodGroeneLijn(string spacer)
+            {
+                tekenRoodBlokje(spacer);
+                tekenGroenBlokje(spacer);
+                tekenGroenBlokje(spacer);
+                tekenRoodBlokje(spacer);
+                Console.WriteLine("");
+            }
+
             // teken lijn 1 (4 rode blokjes)
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("-");
-            Console.Write("-");
-            Console.Write("-");
-            Console.Write("-");
-            Console.WriteLine("-");
+            tekenRodeLijn("-");
 
             // teken lijn 2 & 3 (1 rood blokje, 3 groene blokjes en terug 1 rood blokje)
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("-");
-            Console.ForegroundColor= ConsoleColor.Green;
-            Console.Write("-");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("-");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("-");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("-");
+            for (int i = 0; i < 2; i++)
+            {
+                tekenRoodGroeneLijn("-");
+            }
 
             // teken lijn 4 (4 rode blokjes)
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("-");
-            Console.Write("-");
-            Console.Write("-");
-            Console.Write("-");
-            Console.WriteLine("-");
+            tekenRodeLijn("-");
 
             // console kleur terug naar origineel
             Console.ResetColor();
